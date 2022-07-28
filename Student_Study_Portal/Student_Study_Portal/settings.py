bbe8885033ab732cd60162e370469a87169a928f
AshishBhalotiya1 <97389044+AshishBhalotiya1@users.noreply.github.com>
2022-07-28 23:11:39 +0530

    Update settings.py

--- Student_Study_Portal/Student_Study_Portal/settings.py
+++ Student_Study_Portal/Student_Study_Portal/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m$v4)83hbl8x8h!gk1+7vs0b!x6xc!_32&r8s2pi+bc^%hogk4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["8000"]
 
 
 # Application definition
